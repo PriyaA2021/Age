b8b963e3698679db255ade05636ac200cd7c3dd6
Sripriya Sundararaman <spreiyah@yahoo.com>
2021-06-08 20:26:32 +1200

    Add project files.

---
+++ Age/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            Console.WriteLine("Enter your age");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age > 18)
+            {
+                Console.WriteLine(" You are an adult now");
+            }
+            else
+            {
+                Console.WriteLine("You are minor yet");
+            }
+        }
+    }
+}
